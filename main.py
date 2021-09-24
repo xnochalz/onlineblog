f74e6e30bfedb19cc804eb6945dcb89f1de1f16f
GEE Stone <ngrtnz@gmail.com>
2021-09-24 15:29:47 +0100

    change SQLite to PostgreSQL

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ from forms import RegisterForm, LoginForm, CommentForm, CreatePostForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
-
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
+gravatar = Gravatar(app, size=100, rating='g', default='intro')
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
